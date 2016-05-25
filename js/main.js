bc3f8321d071a9877720383ae93795396175d7df
Isaiah J Turner <github.com@isaiahjturner.com>
2016-05-25 18:49:12 -0400

    lazy load attempt 1

--- js/main.js
+++ js/main.js
@@ -48,7 +48,9 @@ $(document).ready(function() {
         }, 100);
         for (var i = 0; i < imageIds.length; i++) {
             var version = window.devicePixelRatio > 1 ? "@" + window.devicePixelRatio + "x" : "";
-            $imageSet.append("<img src='img/cards/" + imageIds[i] + version + ".png' />");
+            $imageSet.append("<img src='img/cards/" + imageIds[i] + version + ".png' />").lazyload({
+              effect: "fadeIn"
+            });
         }
         $apps.append($imageSet);
         return $imageSet;
